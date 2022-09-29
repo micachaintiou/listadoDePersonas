d6416ed2183366d48474fa94bfc0ceb0c28a6207
Micaela Chaintiou <micachaintiou86@gmail.com>
2022-09-29 20:06:59 -0300

    lista de personas terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Persona } from './persona.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listadoDePersonas';
+  title = 'Listado De Personas';
+  personas: Persona[] = [new Persona('Micaela', 'Chaintiou')]; // new Persona('Laura', 'Juarez')]; //Array para agregar personas
+  nombreInput:string = '';
+  apellidoInput:string = '';
+
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1); //Agrega
+  }
 }
+
+
