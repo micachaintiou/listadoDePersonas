97cfb446c3dd67b3b633ef6d37bb8c7b6099f18a
Micaela Chaintiou <micachaintiou86@gmail.com>
2022-09-30 23:48:59 -0300

    fondo y div cambiado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PersonaComponent,
 
   ],
   imports: [
