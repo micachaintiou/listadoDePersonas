9172fa4b443abeed7f9893f675e238bed722bf9a
Micaela Chaintiou <micachaintiou86@gmail.com>
2022-10-05 17:42:40 -0300

    edicion con angular

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -13,11 +13,11 @@ import { PersonasService } from '../personas.service';
 export class FormularioComponent {
   //@Output() personaCreada= new EventEmitter<Persona>();
 
-  //nombreInput:string = '';
-  //apellidoInput:string = '';
+  nombreInput:string = '';
+  apellidoInput:string = '';
   //personas: any;
-  @ViewChild('nombreInput') nombre:ElementRef;
-  @ViewChild('apellidoInput') apellido:ElementRef;
+  /*@ViewChild('nombreInput') nombre:ElementRef;
+  @ViewChild('apellidoInput') apellido:ElementRef;*/
 
   constructor(private loggingService:LoggingService, private personasService:PersonasService){
   this.personasService.saludar.subscribe((indice:number) => alert("El indice es: " + indice));
@@ -32,7 +32,8 @@ export class FormularioComponent {
   //agregarPersona(nombreInput:HTMLInputElement,apellidoInput:HTMLInputElement){
     //let persona1 = new Persona(nombreInput.value, apellidoInput.value); Es con refeencias locales
     agregarPersona(){
-      let persona1 = new Persona(this.nombre.nativeElement.value, this.apellido.nativeElement.value); //Se usa para recuperar el valor de la plantilla cuando se usa view child
+      let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+      //let persona1 = new Persona(this.nombre.nativeElement.value, this.apellido.nativeElement.value); //Se usa para recuperar el valor de la plantilla cuando se usa view child
       /*this.loggingService.enviarMensajeAConsola("Enviamos persona:" + persona1.nombre + "apellido: " + persona1.apellido);
       this.personaCreada.emit(persona1);*/
       this.personasService.agregarPersona(persona1);
